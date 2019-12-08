31a35f2ef60681fdc48ddef75c3364aa47241d93
Marc Abegon <marc.abegon@gmail.com>
2019-12-08 01:19:57 +0100

    day4_2 (python)

--- day4/python/day4.py
+++ day4/python/day4.py
@@ -14,6 +14,18 @@ def checkNeverDecrease(password):
         current = int(c)
     return True
 
+def checkExactlyTwoAdjacents(password):
+    current = ""
+    occurrences = 1
+    for c in str(password):
+        if c == current:
+            occurrences += 1
+        elif occurrences == 2:
+            return True
+        else:
+            occurrences = 1
+        current = c
+    return occurrences == 2
 
 def solvePuzzle():
     totalPasswords = 0
@@ -22,9 +34,17 @@ def solvePuzzle():
             totalPasswords += 1
     return totalPasswords
 
+def solvePuzzle2():
+    totalPasswords = 0
+    for password in range(165432, 707912):
+        if checkExactlyTwoAdjacents(password) and checkNeverDecrease(password):
+            totalPasswords += 1
+    return totalPasswords
+
 def main():
     result = solvePuzzle()
-    print(str.format("The number of possible password are: {}", result))
+    print(str.format("The number of possible password of Part 1 are: {}", solvePuzzle()))
+    print(str.format("The number of possible password of Part 2 are: {}", solvePuzzle2()))
 
 
 if __name__ == '__main__':
--- day4/python/test_day4.py
+++ day4/python/test_day4.py
@@ -14,3 +14,19 @@ class TestDay4(TestCase):
 
     def test_checkNeverDecrease_when_False(self):
         self.assertEqual(False, day4.checkNeverDecrease(112213))
+
+    def test_checkExactlyTwoAdjacents_when_True(self):
+        self.assertEqual(True, day4.checkExactlyTwoAdjacents(112233))
+
+    def test_checkExactlyTwoAdjacents_when_True2(self):
+        self.assertEqual(True, day4.checkExactlyTwoAdjacents(122446))
+
+    def test_checkExactlyTwoAdjacents_when_True3(self):
+        self.assertEqual(True, day4.checkExactlyTwoAdjacents(122444))
+
+    def test_checkExactlyTwoAdjacents_when_False(self):
+        self.assertEqual(False, day4.checkExactlyTwoAdjacents(123444))
+
+    def test_checkExactlyTwoAdjacents_when_False(self):
+        self.assertEqual(False, day4.checkExactlyTwoAdjacents(688889))
+
